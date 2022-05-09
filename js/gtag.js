727262d2f4f771b6c1ca93b6c84d48c8ba018607
yuchen0515 <i98565412@gmail.com>
2022-05-10 00:31:40 +0800

    Site updated: 2022-05-10 00:31:40

--- js/gtag.js
+++ js/gtag.js
@@ -1,9 +1,10 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJPQK1CYBB"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-CJPQK1CYBB';
+document.head.appendChild(imported);
 
-  gtag('config', 'G-CJPQK1CYBB');
-</script>
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-CJPQK1CYBB');
