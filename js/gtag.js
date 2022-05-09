04c6e6b2d315725dbd55a321c3cb553a97920d36
yuchen0515 <i98565412@gmail.com>
2022-05-10 00:07:51 +0800

    Site updated: 2022-05-10 00:07:51

--- js/gtag.js
+++ js/gtag.js
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CJPQK1CYBB"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJPQK1CYBB"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
